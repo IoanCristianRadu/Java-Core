c488f33209b400d8102586a0065281ec70095549
IoanCristianRadu <raduioancristian@gmail.com>
2019-02-17 16:08:22 +0100

    "Added modifiers"

---
+++ src/modifiers/nonaccess/Static.java
@@ -0,0 +1,27 @@
+package modifiers.nonaccess;
+
+public class Static {
+    private static int numInstances = 0;
+
+    protected static int getCount() {
+        return numInstances;
+    }
+
+    private static void addInstance() {
+        numInstances++;
+    }
+
+    Static() {
+        Static.addInstance();
+    }
+
+    public static void main(String[] arguments) {
+        //Because it is static, it exists before instantiating the class.
+        System.out.println("Starting with " + Static.getCount() + " instances");
+
+        for (int i = 0; i < 500; ++i) {
+            new Static();
+        }
+        System.out.println("Created " + Static.getCount() + " instances");
+    }
+}
