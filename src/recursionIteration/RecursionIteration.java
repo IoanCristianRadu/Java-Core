99e1689d4b1aa4e2b6226ecae1d9ad0ad3873f58
Cristian <raduioancristian@gmail.com>
2019-03-20 11:00:03 +0100

    Refactor recursionIteration

--- src/recursionIteration/RecursionIteration.java
+++ src/recursionIteration/RecursionIteration.java
@@ -9,11 +9,8 @@ public class RecursionIteration {
     private static int fibonacciRecursion(int nthNumber) {
         //use recursion
         if (nthNumber == 0) {
-
             return 0;
-
         } else if (nthNumber == 1) {
-
             return 1;
         }
         return fibonacciRecursion(nthNumber - 1) + fibonacciRecursion(nthNumber - 2);
@@ -21,16 +18,11 @@ public class RecursionIteration {
 
     private static int fibonacciIteration(int nthNumber) {
         //use loop
-        int previouspreviousNumber, previousNumber = 0, currentNumber = 1;
-
+        int previousPreviousNumber, previousNumber = 0, currentNumber = 1;
         for (int i = 1; i < nthNumber; i++) {
-
-            previouspreviousNumber = previousNumber;
-
+            previousPreviousNumber = previousNumber;
             previousNumber = currentNumber;
-
-            currentNumber = previouspreviousNumber + previousNumber;
-
+            currentNumber = previousPreviousNumber + previousNumber;
         }
         return currentNumber;
     }
